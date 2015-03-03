c68d70c522f1456f4b5ca1790839c2b7340a1fae
Hans Hauge <hansthered@gmail.com>
2015-03-03 08:17:07 -0800

    fixes a bug

--- app/models/reddit_funny_article.rb
+++ app/models/reddit_funny_article.rb
@@ -1,24 +1,22 @@
 class RedditFunnyArticle < ActiveRecord::Base
+  attr_reader :content
+
   extend FriendlyId
   friendly_id :title, use: :slugged
 
-  attr_accessor :actual_image
-   validates_uniqueness_of :guid
-
+  validates_uniqueness_of :guid
   validates_presence_of :title, :source, :guid, :content
-
 
   def self.update_from_feed(entries)
     add_entries(entries)
   end
 
-  def actual_image
-    image_url = content.match(/">\[link\]/).pre_match.match(/"/).post_match.match(/.*(href=")/).post_match
+  def self.actual_image(summary = nil)
+    summary_content = summary || content
+    image_url = summary_content.match(/">\[link\]/).pre_match.match(/"/).post_match.match(/.*(href=")/).post_match
     image_url_with_extension = string_is_probably_a_picture?(image_url) ? image_url : image_url + '.png'
     image_url_with_extension.match(/imgur.com\/gallery/) ? image_url_with_extension.gsub('imgur.com/gallery', 'i.imgur.com') : image_url_with_extension
   end
-
-  private
 
   def self.add_entries(entries)
     return unless entries.present?
@@ -28,15 +26,15 @@ class RedditFunnyArticle < ActiveRecord::Base
           :title     => entry.title,
           :content   => entry.summary,
           :source    => get_host_without_www(entry.url),
-          :thumbnail => find_or_create_thumbnail(entry.media_thumbnail_url.first),
+          :thumbnail => find_or_create_thumbnail(entry.media_thumbnail_url.first, entry.summary),
           :guid      => entry.id
         )
       end
     end
   end
 
-  def self.find_or_create_thumbnail(url)
-    url || actual_image
+  def self.find_or_create_thumbnail(url, summary)
+    url || actual_image(summary)
   end
 
   def self.get_host_without_www(url)
@@ -45,7 +43,7 @@ class RedditFunnyArticle < ActiveRecord::Base
     host.start_with?('www.') ? host[4..-1] : host
   end
 
-  def string_is_probably_a_picture?(str)
+  def self.string_is_probably_a_picture?(str)
     image_extensions = %w(.jpg .png .gif .jpeg)
     image_extensions.each do |ext|
       return true if str.ends_with?(ext)
