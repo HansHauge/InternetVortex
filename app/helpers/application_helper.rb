22071b9e316b82b4485f659578dbdaa032f29fd7
Hans Hauge <hansthered@gmail.com>
2015-03-05 10:28:33 -0800

    rearranges the navbar a bit

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -6,4 +6,9 @@ module ApplicationHelper
   def gifv_sub_mp4(str)
     str.ends_with?('.gifv') ? str.gsub('.gifv', '.mp4') : str
   end
+
+  def check_if_active(page_link)
+    controller_name = controller.class.name
+    return "active" if controller_name.include?(page_link)
+  end
 end
