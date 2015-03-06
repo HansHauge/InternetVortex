a4ce6bb8b029d98c604577775446da65364a9f63
Hans Hauge <hansthered@gmail.com>
2015-03-05 16:40:18 -0800

    adds thumbnails to the homepage when necessary, extends main pages to the last 24 hours

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,13 +1,15 @@
 class ArticlesController < ApplicationController
   def index
-    chive_articles = ChiveArticle.last(20)
-    buzzfeed_articles = BuzzfeedArticle.last(20)
-    memebase_articles = MemebaseArticle.last(20)
-    r_funny_articles = RedditFunnyArticle.last(20)
+    time_range = (DateTime.current - 1.day)..DateTime.current
+
+    chive_articles = ChiveArticle.where(created_at: time_range)
+    buzzfeed_articles = BuzzfeedArticle.where(created_at: time_range)
+    memebase_articles = MemebaseArticle.where(created_at: time_range)
+    r_funny_articles = RedditFunnyArticle.where(created_at: time_range)
     articles = chive_articles.concat(buzzfeed_articles).concat(memebase_articles).concat(r_funny_articles)
 
     sorted_articles = articles.sort { |x,y| x.created_at <=> y.created_at }.reverse
-    @articles = sorted_articles.paginate(:page => params[:page], :per_page => 20)
+    @articles = sorted_articles.paginate(:page => params[:page], :per_page => 30)
   end
 
   def show
@@ -33,7 +35,7 @@ class ArticlesController < ApplicationController
     articles = chive_articles.concat(buzzfeed_articles).concat(memebase_articles).concat(r_funny_articles)
 
     sorted_articles = articles.sort { |x,y| x.created_at <=> y.created_at }.reverse
-    @articles = sorted_articles.paginate(:page => params[:page], :per_page => 20)
+    @articles = sorted_articles.paginate(:page => params[:page], :per_page => 30)
     @date_message = @articles.count > 0 ? "Select Date: " : "Try another date:"
 
     flash.now[:warning] = 'No content found...' if @articles.count == 0
--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,18 +1,19 @@
 class WelcomeController < ApplicationController
   def home
-    #articles
-    chive_articles = ChiveArticle.last(20)
-    buzzfeed_articles = BuzzfeedArticle.last(20)
-    memebase_articles = MemebaseArticle.last(20)
-    r_funny_articles = RedditFunnyArticle.last(20)
+    time_range = (DateTime.current - 1.day)..DateTime.current
+
+    chive_articles = ChiveArticle.where(created_at: time_range)
+    buzzfeed_articles = BuzzfeedArticle.where(created_at: time_range)
+    memebase_articles = MemebaseArticle.where(created_at: time_range)
+    r_funny_articles = RedditFunnyArticle.where(created_at: time_range)
     articles = chive_articles.concat(buzzfeed_articles).concat(memebase_articles).concat(r_funny_articles)
 
     #jokes
-    jokes = Joke.last(20)
+    jokes = Joke.where(created_at: time_range)
 
     listings = articles.concat(jokes).sort { |x,y| x.created_at <=> y.created_at }.reverse
 
-    @listings = listings.paginate(:page => params[:page], :per_page => 20)
+    @listings = listings.paginate(:page => params[:page], :per_page => 30)
   end
 
   def about
@@ -39,7 +40,7 @@ class WelcomeController < ApplicationController
     jokes = Joke.where(created_at: time_range)
     listings = articles.concat(jokes).sort { |x,y| x.created_at <=> y.created_at }.reverse
 
-    @listings = listings.paginate(:page => params[:page], :per_page => 20)
+    @listings = listings.paginate(:page => params[:page], :per_page => 30)
     @date_message = @listings.count > 0 ? "Select Date: " : "Try another date:"
 
     flash.now[:warning] = 'No content found...' if @listings.count == 0
