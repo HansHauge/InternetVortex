d5d76f7c7db37772e272cbb14aac00deec8fa31e
Hans Hauge <hansthered@gmail.com>
2015-03-16 11:54:13 -0700

    checks for a video or an image on cheezeburger articles, adds thumbnail for liveleak

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,3 +1,7 @@
 module ApplicationHelper
 
+  def article_is_probably_a_video?(article)
+    thumbnail_setter = ThumbnailSetter.new({})
+    thumbnail_setter.video_thumbnails.include?(article.thumbnail) ? 'embed-responsive embed-responsive-16by9' : 'img-responsive'
+  end
 end
