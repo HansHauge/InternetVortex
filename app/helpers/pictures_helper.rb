3ca5fe8960a7fec660392df29f2664feeb83198f
Hans Hauge <hansthered@gmail.com>
2015-03-18 15:55:42 -0700

    hotfix

--- app/helpers/pictures_helper.rb
+++ app/helpers/pictures_helper.rb
@@ -8,6 +8,18 @@ module PicturesHelper
   end
 
   def no_direct_link?(img)
-    PictureHelper.no_direct_link?(img)
+    imgur_gallery?(img) || live_meme?(img) || img_flip?(img)
+  end
+
+  def imgur_gallery?(str)
+    str.match(/imgur.com\/gallery/) || str.match(/imgur.com\/a\//)
+  end
+
+  def live_meme?(str)
+    str.match(/livememe.com/)
+  end
+
+  def img_flip?(str)
+    str.match(/imgflip.com/)
   end
 end
